2b50337348936ee4ce65ed6f476a1ea61ce6c0ea
缪缪 <3584527102@qq.com>
2025-06-12 09:24:41 +0800

    V1.2补充更新

--- static/js/启动.js
+++ static/js/启动.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hasVisited = localStorage.getItem('hasVisited');
     const 进入博客 = document.querySelector('.loading');
-    const loading状态 = document.querySelector('.loading方块');
+    const loading方块 = document.querySelector('.loading方块');
     let 动画执行次数 = 0;
     if (!hasVisited)  {
         localStorage.setItem('hasVisited', 'true');
     } else {
         hideLoading(进入博客);
     }
-    loading状态.addEventListener('animationiteration', () => {
+    loading方块.addEventListener('animationiteration', () => {
         动画执行次数++;
         if (动画执行次数 === 2) {
-            loading状态.style.animation = 'none'; 
-            loading状态.parentElement.style.display = 'none';
             hideLoading(进入博客);
         }
     });
 });
 function hideLoading(进入博客) {
+    const loading动画 = document.querySelector('.loading动画');
+    loading动画.style.transform = 'translate(-50%, -50%) scale(.7)';
     进入博客.style.opacity = '0';
     setTimeout(() => {
         进入博客.style.display = 'none';
